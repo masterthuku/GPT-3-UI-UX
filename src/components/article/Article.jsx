928c6335bdde4a71c8bb302587f22ff44a37997f
masterthuku <peterthuku706@gmail.com>
2024-02-13 15:03:10 +0300

    update

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -6,7 +6,7 @@ const Article = ({imgUrl, date, title}) => {
   return (
     <div className='gpt3__blog-container_article'>
       <div className='gpt3__blog-container_article-image'>
-        <img src={imgUrl} alt='blog image' />
+        <img src={imgUrl} alt='blog' />
       </div>
       <div className='gpt3__blog-container_article-content'>
         <div>
